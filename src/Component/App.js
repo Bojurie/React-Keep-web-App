42517767e5a123af4c1613a7d5795694c2d97d7c
Bojurie Rogers-Wright <40696649+Bojurie@users.noreply.github.com>
2020-02-29 21:26:55 -0800

    complete app

---
+++ src/Component/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Component/Header'
+import Footer from '../Component/Footer';
+import Note from './Note';
+
+function App (){
+  return(
+    <div>
+      <Header />
+      <Note />
+      <Footer/>
+    </div>
+  )
+}
+export default App;
